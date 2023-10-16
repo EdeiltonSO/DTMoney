e9584cadae621579657f0bfca3043a3163e80413
Edeilton Oliveira <edeiltonso@gmail.com>
2023-10-16 02:39:35 -0300

    refact: implementa use-context-selector para melhorar desempenho nas renderizações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react'
+// import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { Header } from '../../components/Header'
 import { SearchForm } from '../../components/SearchForm'
 import { Summary } from '../../components/Summary'
@@ -11,7 +12,9 @@ import { TransactionsContext } from '../../contexts/Transaction'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>
