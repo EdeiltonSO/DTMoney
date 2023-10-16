895e1f11d3298e79d138e8a05f0023699120506c
Edeilton Oliveira <edeiltonso@gmail.com>
2023-10-16 14:26:08 -0300

    refact: implementa useMemo no reduce do summary

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,28 +1,31 @@
 import { TransactionsContext } from '../contexts/Transaction'
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.price
+          accumulator.total += transaction.price
+        } else {
+          accumulator.outcome += transaction.price
+          accumulator.total -= transaction.price
+        }
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
